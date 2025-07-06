8b04710999705c987fe44e05ad0f6d12bbe11dc1
Srinath10X <srinathcreates@gmail.com>
2025-07-06 13:52:16 +0530

    feat: added routes in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import HomePage from "./pages/HomePage";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+
+import { Route, Routes } from "react-router-dom";
+
 const App = () => {
   return (
-    <>
-      <div>App</div>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signin" element={<SignInPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+    </Routes>
   );
 };
 
